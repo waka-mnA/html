681ff142ecaffcd505ccc9ed40dd3aa1d6efa3b2
Wakako Kurata <cprc_lr_hysks@yahoo.co.jp>
2017-01-04 21:24:59 +0000

    pass

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
   <!-- jQuery Reading -->
   <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> -->
   <!-- Bootstrap JS Reading -->
-  <!-- <script src="js/bootstrap.min.js"></script> -->
+  <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
   Hello World!
