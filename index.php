6e4a74e784ebe75eed50be49944bd6288f380270
Wakako Kurata <cprc_lr_hysks@yahoo.co.jp>
2017-01-04 21:25:19 +0000

    pass

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <!-- Bootstrap CSS Reading -->
   <!-- <link href="css/bootstrap.min.css" rel="stylesheet"> -->
   <!-- jQuery Reading -->
-  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <!-- Bootstrap JS Reading -->
   <script src="js/bootstrap.min.js"></script>
 </head>
