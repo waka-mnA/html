6d3b6d36ff743b127438dd243e9f7d3cdf5397ff
Wakako Kurata <cprc_lr_hysks@yahoo.co.jp>
2017-01-05 05:01:42 +0000

    upload

--- upload.php
+++ upload.php
@@ -6,32 +6,33 @@ use Aws\Common\Enum\Region;
 use Aws\S3\Exception\S3Exception;
 use Guzzle\Http\EntityBody;
 
+// Bucket name
+$bucket = "comsm0010-wk13290";
+// Filename to be uploaded
+$key = "sakura.jpg";
+
+
 $client = S3Client::factory(array(
   "key" => "AKIAJNH4SBIA7MP6SB2Q",
   "secret" => "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",
   "region" => Region::EU-WEST-2
 ));
 
-$tmpfile = $_FILES["upfile"]["tmp_name"];
-
-if (!is_uploaded_file($tmpfile)) {
-    die('File is not uploaded');
-}
-
-// Bucket name
-$bucket = "comsm0010-wk13290";
-// Filename to be uploaded
-$key = "sakura.jpg";
-
-try {
-    $result = $client->putObject(array(
-        'Bucket' => $bucket,
-        'Key' => $key,
-        'Body' => EntityBody::factory(fopen($tmpfile, 'r')),
-    ));
-    echo "Uploaded Successfully!";
-} catch (S3Exception $exc) {
-    echo "Upload Failed.";
-    echo $exc->getMessage();
-}
+// $tmpfile = $_FILES["upfile"]["tmp_name"];
+//
+// if (!is_uploaded_file($tmpfile)) {
+//     die('File is not uploaded');
+// }
+//
+// try {
+//     $result = $client->putObject(array(
+//         'Bucket' => $bucket,
+//         'Key' => $key,
+//         'Body' => EntityBody::factory(fopen($tmpfile, 'r')),
+//     ));
+//     echo "Uploaded Successfully!";
+// } catch (S3Exception $exc) {
+//     echo "Upload Failed.";
+//     echo $exc->getMessage();
+// }
 ?>
