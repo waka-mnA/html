81943483bcfb5dbcfd17fe21ce89417d67500c94
Wakako Kurata <cprc_lr_hysks@yahoo.co.jp>
2017-01-05 05:47:07 +0000

    upload

--- db.php
+++ db.php
@@ -3,11 +3,11 @@ $server = "localhost";
 $user = "root";
 $password = "Hysks4212";
 $dbname = "member_test";
-
+echo "TEST0";
 /*Connect to database*/
 $conn = mysqli_connect($server, $user, $password, $dbname);
 // mysql_select_db($dbname);
-
+echo "TEST";
 if(mysqli_connect_errno() > 0){
   echo "MYSQLI FAILED.";
   exit;
