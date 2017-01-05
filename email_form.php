d140890a2cbf17db78670677bbb145edd907bff7
Wakako Kurata <cprc_lr_hysks@yahoo.co.jp>
2017-01-05 00:43:19 +0000

    user

--- email_form.php
+++ email_form.php
@@ -1,9 +1,9 @@
 <form action="index.php" method="post"><input type="hidden" name="mode" value="email_regist" />
-  <table><caption>メールアドレス登録フォーム</caption>
+  <table><caption>E-mail Address Registration Form</caption>
     <tr>
-      <td class="item">E-mail：</td>
+      <td class="item">E-mail:</td>
       <td><input type="text" name="email" size="40" /></td>
     </tr>
   </table>
-  <div><input type="submit" name="submit" value="送信" /></div>
+  <div><input type="submit" name="submit" value="Submit" /></div>
 </form>
