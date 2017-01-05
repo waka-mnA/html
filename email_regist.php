0dc89ed71faec4846bfe20684274757d66c56209
Wakako Kurata <cprc_lr_hysks@yahoo.co.jp>
2017-01-05 01:28:15 +0000

    user

--- email_regist.php
+++ email_regist.php
@@ -15,7 +15,7 @@ else{
 
   //SQL
   $query = "insert into members (pre_userid, email) values('$pre_user_id'. '$email')";
-  $result = mysqli_query($query);
+  $result = mysqli_query($query)or die(mysqli_error($db));
 
   //Check it is registered successfully
   if($result == false) {
