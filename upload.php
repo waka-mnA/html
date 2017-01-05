aebde3a08bff55ef82072cd569f5088cd2a399bd
Wakako Kurata <cprc_lr_hysks@yahoo.co.jp>
2017-01-05 19:24:25 +0000

    upload

--- upload.php
+++ upload.php
@@ -1,39 +1,38 @@
 <?php
-echo "TEST1";
   require("aws.phar");
-//require("phar://".dirname(__FILE__)."aws.phar/aws-autoloader.php");
-use Aws\S3\S3Client;
-use Aws\Common\Enum\Region;
-use Aws\S3\Exception\S3Exception;
-use Guzzle\Http\EntityBody;
-echo "TEST";
-// Bucket name
-$bucket = "comsm0010-wk13290";
-// Filename to be uploaded
-$key = "sakura.jpg";
+  //require("phar://".dirname(__FILE__)."aws.phar/aws-autoloader.php");
+  use Aws\S3\S3Client;
+  use Aws\Common\Enum\Region;
+  use Aws\S3\Exception\S3Exception;
+  use Guzzle\Http\EntityBody;
+
+  // Bucket name
+  $bucket = "comsm0010-wk13290";
+  // Filename to be uploaded
+  $key = "sakura.jpg";
 
 
-// $client = S3Client::factory(array(
-//   "key" => "AKIAJNH4SBIA7MP6SB2Q",
-//   "secret" => "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",
-//   "region" => Region::EU-WEST-2
-// ));
+  $client = S3Client::factory(array(
+    "key" => "AKIAJNH4SBIA7MP6SB2Q",
+    "secret" => "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",
+    "region" => Region::EU-WEST-2
+  ));
 
-// $tmpfile = $_FILES["upfile"]["tmp_name"];
-//
-// if (!is_uploaded_file($tmpfile)) {
-//     die('File is not uploaded');
-// }
-//
-// try {
-//     $result = $client->putObject(array(
-//         'Bucket' => $bucket,
-//         'Key' => $key,
-//         'Body' => EntityBody::factory(fopen($tmpfile, 'r')),
-//     ));
-//     echo "Uploaded Successfully!";
-// } catch (S3Exception $exc) {
-//     echo "Upload Failed.";
-//     echo $exc->getMessage();
-// }
+  $tmpfile = $_FILES["upfile"]["tmp_name"];
+
+  if (!is_uploaded_file($tmpfile)) {
+      die('File is not uploaded');
+  }
+
+  try {
+      $result = $client->putObject(array(
+          'Bucket' => $bucket,
+          'Key' => $key,
+          'Body' => EntityBody::factory(fopen($tmpfile, 'r')),
+      ));
+      echo "Uploaded Successfully!";
+  } catch (S3Exception $exc) {
+      echo "Upload Failed.";
+      echo $exc->getMessage();
+  }
 ?>
