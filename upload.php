3a9426a858e01c2aa450fa47c4c3f31fb09947f9
Wakako Kurata <cprc_lr_hysks@yahoo.co.jp>
2017-01-06 00:57:00 +0000

    upload

--- upload.php
+++ upload.php
@@ -20,36 +20,49 @@
 // ]);
 // $client = $sdk->createS3();
 
-$client = S3Client::factory(array(
-    "key" => getenv("AWS_ACCESS_KEY_ID"),
-    "secret" => getenv("AWS_SECRET_ACCESS_KEY"),
-    "region" => 'eu-west-2',
-    'profile' => 'default',
-    'version' => 'latest',
-));
+// $client = S3Client::factory(array(
+//     "key" => getenv("AWS_ACCESS_KEY_ID"),
+//     "secret" => getenv("AWS_SECRET_ACCESS_KEY"),
+//     "region" => 'eu-west-2',
+//     'profile' => 'default',
+//     'version' => 'latest',
+// ));
+$s3 = new S3(getenv("AWS_ACCESS_KEY_ID"), getenv("AWS_SECRET_ACCESS_KEY"));
 
 
 //Get file name
 $filepath = $_FILES["upfile"]["tmp_name"];
 $type = $_FILES['upfile']["type"];
 $filepath="sakura1.jpg";
+
+$fileName = $_FILES['upfile']['name'];
+
 // if (!is_uploaded_file($filepath)) {
 //   die('File is not uploaded');
 // }
-echo getenv('AWS_SECRET_ACCESS_KEY');
+
 try {
   echo "TEST1";
-  $result = $client->putObject([
-      'Bucket' => $bucket,
-      'Key' => $key,
-      'Body' => file_get_contents($filepath),
-      //EntityBody::factory(fopen($filepath, 'r')),
-      //'SourceFile'   => $filepath,
-      'ACL'          => 'public-read',
-      'ContentType' => $type,
-  ]);
 
-    echo "Uploaded Successfully!";
+
+  $s3->putBucket("comsm0010-wk13290", S3::ACL_PUBLIC_READ);
+
+//move the file
+if ($s3->putObjectFile($filepath, $bucket, $fileName, S3::ACL_PUBLIC_READ)) {
+    echo "We successfully uploaded your file.";
+}else{
+    echo "Something went wrong while uploading your file... sorry.";
+}
+
+  // $result = $client->putObject([
+  //     'Bucket'    => $bucket,
+  //     'Key'       => $key,
+  //     'Body'      => file_get_contents($filepath),
+  //     'ACL'       => 'public-read',
+  //     'ContentType' => $type,
+  // ]);
+
+  echo "Uploaded Successfully!";
 } catch (S3Exception $exc) {
     echo "Upload Failed.";
     echo $exc->getMessage();
