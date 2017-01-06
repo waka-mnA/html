04bb9bdad15c7ad45ca3240f4ae7679f2d97be80
Wakako Kurata <cprc_lr_hysks@yahoo.co.jp>
2017-01-06 01:09:40 +0000

    upload

--- upload.php
+++ upload.php
@@ -29,10 +29,11 @@
 //     'profile' => 'default',
 //     'version' => 'latest',
 // ));
-$s3 = S3Client::factory((array(
+$s3 = S3Client::factory([
   "key" => getenv("AWS_ACCESS_KEY_ID"),
   "secret" => getenv("AWS_SECRET_ACCESS_KEY"),
-   "region" => 'eu-west-2', ));
+   "region" => 'eu-west-2',
+ ]);
 
 
 //Get file name
