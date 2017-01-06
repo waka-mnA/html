dad8c6cf1f61c1ef123d1f4b26d3219ceee44ad2
Wakako Kurata <cprc_lr_hysks@yahoo.co.jp>
2017-01-06 03:39:35 +0000

    display

--- main.php
+++ main.php
@@ -99,6 +99,7 @@
 			//echo"<td width=\"200\">".$filename . "</td>";
 			echo "<td><a href=" .$filename . "><img src = " .$filename. "></a>".$array[$i] . "</td>";
 
+
 			$cnt = $cnt + 1;
 
 			if ($cnt >= $max) {
