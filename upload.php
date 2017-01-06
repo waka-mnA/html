c69eba056e4166aa1206310350b553068613feab
Wakako Kurata <cprc_lr_hysks@yahoo.co.jp>
2017-01-06 01:20:04 +0000

    upload

--- upload.php
+++ upload.php
@@ -32,8 +32,8 @@
 // ));
 
 try{
-  if(is_uploaded_file($_FILES['file']['tmp_name'])){
-    move_uploaded_file($_FILES['file']['tmp_name'], $bucketpath.$key);
+  if(is_uploaded_file($_FILES['upfile']['tmp_name'])){
+    move_uploaded_file($_FILES['upfile']['tmp_name'], $bucketpath.$key);
     echo 'Uploaded Successfully!';
   }
 
